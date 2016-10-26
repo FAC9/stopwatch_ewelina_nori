34d27dc0eaf98164c49b52eddc60376948173757
denesnori <denes.nori@yahoo.com>
2016-10-26 13:38:56 +0100

    test for stopTimer func

--- stop-watch.js
+++ stop-watch.js
@@ -3,4 +3,10 @@ function StopWatch () {
   this.timeElapsed = 0;
   this.startTime = 0;
 
+};
+
+StopWatch.prototype.startTimer = function(startTime){
+    var timerStarted = startTime; // argument externally supplied
+    console.log("startTime: "+startTime);
+    return timerStarted;
 }

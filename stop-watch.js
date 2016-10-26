1ff5072db0906f350cc23c9b22f95d32f2b51f21
denesnori <denes.nori@yahoo.com>
2016-10-26 15:36:18 +0100

    ms converter

--- stop-watch.js
+++ stop-watch.js
@@ -25,3 +25,40 @@ StopWatch.prototype.resetTimer = function () {
   document.getElementsByTagName('time')[0].innerHTML = "00:00:00:00";
   return this.timeElapsed;
 }
+
+StopWatch.prototype.renderTimer = function(elapsed){
+	var hours=0,minutes=0,seconds=0 ,milliseconds = 0,res;
+//	elapsed=4620000
+		//1ms
+	milliseconds= Math.floor((elapsed%1000)/10);
+	if(milliseconds < 10)
+		milliseconds = '0' + milliseconds;
+	//1s 1000ms
+	if(elapsed > 1000){
+		seconds = Math.floor(elapsed/1000)%60;
+		if(seconds < 10)
+			seconds = '0' + seconds;
+	}else{
+		seconds = '00';
+	}
+	//1m 60 000s
+	if(elapsed > 60000){
+		minutes = Math.floor(elapsed/60000)%60;
+		if(minutes < 10)
+			minutes = '0' + minutes;
+	}else{
+		minutes = '00';
+	}
+	//1h = 3 600 000ms
+	if(elapsed > 3600000){
+		hours = Math.floor(elapsed / 3600000);
+			if(hours < 10)
+			hours = '0' + hours;
+	}else{
+		hours= '00';
+	}
+	res = hours + ':'+minutes+':'+seconds+':'+milliseconds;
+	console.log(hours,minutes,seconds,milliseconds);
+	console.log(res);
+	return res;
+}

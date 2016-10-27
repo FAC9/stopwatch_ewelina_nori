68237fc530c1b9bd7e5e3be5ad32870ed4577505
ewelina <skibinska.ewelina@gmail.com>
2016-10-27 14:14:33 +0100

    Old stopwatch

--- run-stop-watch.js
+++ run-stop-watch.js
@@ -1,9 +1,10 @@
 'use strict';
 var stopWatch = new StopWatch();
+console.log(stopWatch);
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+var reset = document.getElementById("reset");
 
- var start = document.getElementById("start");
- var stop = document.getElementById("stop");
- var reset = document.getElementById("reset");
-
-start.addEventListener("click", stopWatch.startTimer);
-reset.addEventListener("click", stopWatch.resetTimer);
+start.addEventListener("click", function(){stopWatch.startTimer()},false);
+stop.addEventListener("click",function(){stopWatch.stopTimer()},false);
+reset.addEventListener("click", function(){stopWatch.resetTimer()},false);
--- stop-watch.js
+++ stop-watch.js
@@ -1,5 +1,5 @@
-  'use strict';
 
+  'use strict';
 var StopWatch = function() {
   this.lock = false;
   this.isStopped = true;
@@ -24,7 +24,7 @@ StopWatch.prototype.stopTimer = function () {
   }
 };
 
-stopWatch.presetTimer = function () {
+StopWatch.prototype.resetTimer = function () {
   this.timeElapsed = 0;
   this.startTime = 0;
   this.laps = [];
@@ -35,82 +35,83 @@ stopWatch.presetTimer = function () {
 }
 
 StopWatch.prototype.renderTimer = function(){
-	var hours=0,minutes=0,seconds=0 ,milliseconds = 0,res;
+    var hours=0,minutes=0,seconds=0 ,milliseconds = 0,res;
   //console.log("IS anybody home?",this);
 
   var elapsed = new Date().getTime() - this.startTime;
-		//1ms
-	milliseconds= Math.floor((elapsed%1000)/10);
-	if(milliseconds < 10)
-		milliseconds = '0' + milliseconds;
-	//1s 1000ms
-	if(elapsed > 1000){
-		seconds = Math.floor(elapsed/1000)%60;
-		if(seconds < 10)
-			seconds = '0' + seconds;
-	}else{
-		seconds = '00';
-	}
-	//1m 60 000s
-	if(elapsed > 60000){
-		minutes = Math.floor(elapsed/60000)%60;
-		if(minutes < 10)
-			minutes = '0' + minutes;
-	}else{
-		minutes = '00';
-	}
-	//1h = 3 600 000ms
-	if(elapsed > 3600000){
-		hours = Math.floor(elapsed / 3600000);
-			if(hours < 10)
-			hours = '0' + hours;
-	}else{
-		hours= '00';
-	}
-	res = hours + ':'+minutes+':'+seconds+':'+milliseconds;
-	//console.log(hours,minutes,seconds,milliseconds);
-//	console.log(res);
+        //1ms
+    milliseconds= Math.floor((elapsed%1000)/10);
+    if(milliseconds < 10)
+        milliseconds = '0' + milliseconds;
+    //1s 1000ms
+    if(elapsed > 1000){
+        seconds = Math.floor(elapsed/1000)%60;
+        if(seconds < 10)
+            seconds = '0' + seconds;
+    }else{
+        seconds = '00';
+    }
+    //1m 60 000s
+    if(elapsed > 60000){
+        minutes = Math.floor(elapsed/60000)%60;
+        if(minutes < 10)
+            minutes = '0' + minutes;
+    }else{
+        minutes = '00';
+    }
+    //1h = 3 600 000ms
+    if(elapsed > 3600000){
+        hours = Math.floor(elapsed / 3600000);
+            if(hours < 10)
+            hours = '0' + hours;
+    }else{
+        hours= '00';
+    }
+    res = hours + ':'+minutes+':'+seconds+':'+milliseconds;
+    //console.log(hours,minutes,seconds,milliseconds);
+//    console.log(res);
   document.getElementsByClassName("timer")[0].innerHTML = res;
-	return res;
+    return res;
 }
+
 StopWatch.prototype.renderTimer2 = function(time){
-	var hours=0,minutes=0,seconds=0 ,milliseconds = 0,res;
+    var hours=0,minutes=0,seconds=0 ,milliseconds = 0,res;
   //console.log("IS anybody home?",this);
 
   var elapsed = time;
-		//1ms
-	milliseconds= Math.floor((elapsed%1000)/10);
-	if(milliseconds < 10)
-		milliseconds = '0' + milliseconds;
-	//1s 1000ms
-	if(elapsed > 1000){
-		seconds = Math.floor(elapsed/1000)%60;
-		if(seconds < 10)
-			seconds = '0' + seconds;
-	}else{
-		seconds = '00';
-	}
-	//1m 60 000s
-	if(elapsed > 60000){
-		minutes = Math.floor(elapsed/60000)%60;
-		if(minutes < 10)
-			minutes = '0' + minutes;
-	}else{
-		minutes = '00';
-	}
-	//1h = 3 600 000ms
-	if(elapsed > 3600000){
-		hours = Math.floor(elapsed / 3600000);
-			if(hours < 10)
-			hours = '0' + hours;
-	}else{
-		hours= '00';
-	}
-	res = hours + ':'+minutes+':'+seconds+':'+milliseconds;
-	//console.log(hours,minutes,seconds,milliseconds);
-//	console.log(res);
+        //1ms
+    milliseconds= Math.floor((elapsed%1000)/10);
+    if(milliseconds < 10)
+        milliseconds = '0' + milliseconds;
+    //1s 1000ms
+    if(elapsed > 1000){
+        seconds = Math.floor(elapsed/1000)%60;
+        if(seconds < 10)
+            seconds = '0' + seconds;
+    }else{
+        seconds = '00';
+    }
+    //1m 60 000s
+    if(elapsed > 60000){
+        minutes = Math.floor(elapsed/60000)%60;
+        if(minutes < 10)
+            minutes = '0' + minutes;
+    }else{
+        minutes = '00';
+    }
+    //1h = 3 600 000ms
+    if(elapsed > 3600000){
+        hours = Math.floor(elapsed / 3600000);
+            if(hours < 10)
+            hours = '0' + hours;
+    }else{
+        hours= '00';
+    }
+    res = hours + ':'+minutes+':'+seconds+':'+milliseconds;
+    //console.log(hours,minutes,seconds,milliseconds);
+//    console.log(res);
 //  document.getElementsByClassName("timer")[0].innerHTML = res;
-	return res;
+    return res;
 }
 StopWatch.prototype.startTimer = function(){
     //console.log("Am I herenow?",this);

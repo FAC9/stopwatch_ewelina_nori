099182c59a62133b771dd7695033dbabf0aa58bd
ewelina <skibinska.ewelina@gmail.com>
2016-10-27 18:01:50 +0100

    Increase readability

--- stop-watch.js
+++ stop-watch.js
@@ -1,5 +1,5 @@
 
-  'use strict';
+'use strict';
 var StopWatch = function() {
   this.lock = false;
   this.isStopped = true;
@@ -10,11 +10,12 @@ var StopWatch = function() {
 };
 
 StopWatch.prototype.stopTimer = function (time) {
-  if (this.lock) return false;
-  else {
-    if(time){
-    this.timeElapsed = time - this.startTime;
-    }else {
+  if (this.lock) {
+    return false;
+  } else {
+    if(time) {
+      this.timeElapsed = time - this.startTime;
+    } else {
       this.timeElapsed = (this.startTime== 0 ? 0:new Date().getTime() - this.startTime);
     }
     this.lock = true;
@@ -29,68 +30,78 @@ StopWatch.prototype.stopTimer = function (time) {
 
 StopWatch.prototype.resetTimer = function () {
   if(this.isStopped === false )
-    this.stopTimer();
+  this.stopTimer();
   this.timeElapsed = 0;
   this.startTime = 0;
   this.isStopped = true
   this.laps = [];
   document.getElementsByClassName('laps')[0].innerHTML = "00:00:00:00";
-  console.log("reset",this.timeElapsed,this.startTime);
   document.getElementsByTagName('time')[0].innerHTML = "00:00:00:00";
   return this.timeElapsed;
 }
 
 StopWatch.prototype.renderTimer = function(time){
-    var hours=0,minutes=0,seconds=0 ,milliseconds = 0,res;
+  var hours = 0,
+  minutes = 0,
+  seconds = 0,
+  milliseconds = 0,
+  res;
+  this.timeElapsed = (time ? time : new Date().getTime() - this.startTime);
 
-  //var elapsed = time;
-  var elapsed = (time?time:new Date().getTime() - this.startTime);
-  this.timeElapsed = (time?time:new Date().getTime() - this.startTime);
-        //1ms
-    milliseconds= Math.floor((elapsed%1000)/10);
-    if(milliseconds < 10)
-        milliseconds = '0' + milliseconds;
-    //1s 1000ms
-    if(elapsed > 1000){
-        seconds = Math.floor(elapsed/1000)%60;
-        if(seconds < 10)
-            seconds = '0' + seconds;
-    }else{
-        seconds = '00';
+  //1ms
+  milliseconds = Math.floor((this.timeElapsed % 1000) / 10);
+  if (milliseconds < 10) {
+    milliseconds = '0' + milliseconds;
+  }
+
+  //1s 1000ms
+  if (this.timeElapsed > 1000) {
+    seconds = Math.floor(this.timeElapsed / 1000) % 60;
+    if(seconds < 10) {
+      seconds = '0' + seconds;
     }
-    //1m 60 000s
-    if(elapsed > 60000){
-        minutes = Math.floor(elapsed/60000)%60;
-        if(minutes < 10)
-            minutes = '0' + minutes;
-    }else{
-        minutes = '00';
+  } else {
+    seconds = '00';
+  }
+
+  //1m 60 000s
+  if(this.timeElapsed > 60000) {
+    minutes = Math.floor(this.timeElapsed / 60000) % 60;
+    if(minutes < 10) {
+      minutes = '0' + minutes;
     }
-    //1h = 3 600 000ms
-    if(elapsed > 3600000){
-        hours = Math.floor(elapsed / 3600000);
-            if(hours < 10)
-            hours = '0' + hours;
-    }else{
-        hours= '00';
+  } else {
+    minutes = '00';
+  }
+
+  //1h = 3 600 000ms
+  if(this.timeElapsed > 3600000){
+    hours = Math.floor(this.timeElapsed / 3600000);
+    if(hours < 10) {
+      hours = '0' + hours;
     }
-    res = hours + ':'+minutes+':'+seconds+':'+milliseconds;
-    return res;
+  } else {
+    hours= '00';
+  }
+  res = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
+  return res;
 }
-StopWatch.prototype.startTimer = function(startT){
-    if (this.lock || !this.isStopped) return false;
-    else {
-      if(startT){
-        this.startTime = startT;
-      }else
-        this.startTime = ( this.startTime ? this.startTime : new Date().getTime());
-      this.lock = true;
-      this.isStopped = false;
-      var test=this;
-      this.interval = setInterval(function(){
-          document.getElementsByClassName("timer")[0].innerHTML = test.renderTimer();
-        },50);
-      this.lock = false;
-      return  this.startTime;
+StopWatch.prototype.startTimer = function(start){
+  if (this.lock || !this.isStopped) {
+    return false;
+  } else {
+    if(start) {
+      this.startTime = start;
+    } else {
+      this.startTime = ( this.startTime ? this.startTime : new Date().getTime());
     }
+    this.lock = true;
+    this.isStopped = false;
+    var test = this;
+    this.interval = setInterval(function(){
+      document.getElementsByClassName("timer")[0].innerHTML = test.renderTimer();
+    },50);
+    this.lock = false;
+    return this.startTime;
+  }
 };
